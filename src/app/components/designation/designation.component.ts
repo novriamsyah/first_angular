c1ecdc0673eb36c81e0402b90a39f8b705334fe7
novriamsyah <novriamsyahh@gmail.com>
2024-12-24 16:41:31 +0700

    add select2 and add service get designation

--- src/app/components/designation/designation.component.ts
+++ src/app/components/designation/designation.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ApiResponseModel, IDesignation } from '../../model/interface/role';
+import { MasterService } from '../../services/master.service';
 
 @Component({
   selector: 'app-designation',
@@ -7,6 +9,20 @@ import { Component } from '@angular/core';
   templateUrl: './designation.component.html',
   styleUrl: './designation.component.css'
 })
-export class DesignationComponent {
+export class DesignationComponent implements OnInit {
+
+  designationList : IDesignation[] = [];
+  masterService = inject(MasterService);
+
+ngOnInit(): void {
+    this.masterService.getDesignation().subscribe((data : ApiResponseModel) => {
+      this.designationList = data.data
+      }, (error) =>{ 
+        alert("Data gagal diambil");
+      }
+    );
+}
+
+
 
 }
--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,31 +1,70 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ApiResponseModel, IRole } from '../../model/interface/role';
+import { CommonModule } from '@angular/common';
+import { SelectDropDownModule } from 'ngx-select-dropdown'
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule, SelectDropDownModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
-  firstName : String = "Jhon Doe";
+export class RolesComponent implements OnInit{
 
-  versionAngular = "Angular 18";
+  http = inject(HttpClient);
+  roleList : IRole[] = [];
+  selectedRole: IRole | null = null;
 
-  version : number = 14;
-
-  dateTime : Date = new Date();
-
-  inputType = "checkbox";
-
-  selectedState = "";
-
-  showAlert() {
-    alert("Perhatian ini alert !")
+  dropdownConfig = {
+    displayKey: "role",
+    search: true,
+    height: '300px',
+    placeholder: 'Select a role',
+    customComparator: (a: any, b: any) => {
+      if (a.roleId < b.roleId) {
+        return -1;
+      } else if (a.roleId > b.roleId) {
+        return 1;
+      } else {
+        return 0;
+      }
+    },
+    limitTo: 0,
+    clearOnSelection: false,
+    inputDirection: 'ltr'
   }
 
-  showAlertMsg(msg : String) {
-    alert(msg)
+  ngOnInit(): void {
+      this.getAllRoles();
   }
+
+  getAllRoles() {
+    this.http.get<ApiResponseModel>("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles").subscribe((res : ApiResponseModel) => {
+      this.roleList = res.data;
+    })
+  }
+
+
+  // firstName : String = "Jhon Doe";
+
+  // versionAngular = "Angular 18";
+
+  // version : number = 14;
+
+  // dateTime : Date = new Date();
+
+  // inputType = "checkbox";
+
+  // selectedState = "";
+
+  // showAlert() {
+  //   alert("Perhatian ini alert !")
+  // }
+
+  // showAlertMsg(msg : String) {
+  //   alert(msg)
+  // }
 }
